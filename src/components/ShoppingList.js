a1209f413cb52684a54cfaf59dbd44b53c425a5c
joannayhs <joannayuhas@gmail.com>
2022-02-17 16:57:08 -0500

    made necessary changes to pass all tests

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  const filteredItems = selectedCategory != "All" ? items.filter(item => item.category === selectedCategory) : items
+
+  function handleChange(e){
+    setSelectedCategory(e.target.value)
+  }
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleChange} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +21,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
